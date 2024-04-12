d867caf0125968544107e788183898ffdfedeb92
Birhanu <birhanuasmamaw24@gmail.com>
2024-04-12 11:13:51 -0900

    adding background image for homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,34 +4,39 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MY Food'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/deliver.png',
-              width: 300,
-              height: 300,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'MY Food',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/back.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/deliver.png',
+                width: 300,
+                height: 300,
               ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Deliver Favorite Food',
-              style: TextStyle(
-                fontSize: 18,
+              SizedBox(height: 20),
+              Text(
+                'MY Food',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 10),
+              Text(
+                'Deliver Favorite Food',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
