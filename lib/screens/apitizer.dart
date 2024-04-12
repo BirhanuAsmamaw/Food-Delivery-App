b39d98fc15de63cb6ca687049c89eecf7f149603
Birhanu <birhanuasmamaw24@gmail.com>
2024-04-12 11:13:14 -0900

    adding background image for the code

--- lib/screens/apitizer.dart
+++ lib/screens/apitizer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/screens/home_screen.dart';
 
 class Apitizer extends StatelessWidget {
   @override
@@ -8,7 +9,12 @@ class Apitizer extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 30),
+            Image.asset(
+              'assets/burger.png',
+              width: 300,
+              height: 300,
+            ),
+            SizedBox(height: 10), // Adjust the gap between the image and the first text
             Text(
               'Find your Comfort Food here',
               style: TextStyle(
@@ -16,21 +22,38 @@ class Apitizer extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Text(
-              'Here you can find a chef or dish for every taste and color. Enjoy!',
-              style: TextStyle(
-                fontSize: 15,
+            SizedBox(height: 20), // Add spacing between the first and second text
+            Center( // Center the text horizontally
+              child: Text(
+                'Here you can find a chef or dish for every taste and color. Enjoy!', // Display the text in two lines
+                textAlign: TextAlign.center, // Center the text horizontally
+                style: TextStyle(
+                  fontSize: 15,
+                ),
               ),
             ),
+            SizedBox(height: 30), // Add spacing between the second text and the button
             ElevatedButton(
-          child: const Text("Next"),
-          onPressed: () {
-            Navigator.pushNamed(context, '/home_Screen');
-          },
-        ),
+              onPressed: () {
+                Navigator.push(
+                 context,
+                 MaterialPageRoute(
+                     builder: (BuildContext context) => HomeScreen()));
+              },
+              style: ElevatedButton.styleFrom(
+                // primary: Colors.blue, // Background color
+                // onPrimary: Colors.white, // Text color
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // Rounded corners
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20), // Add padding to make the button wider
+              ),
+              child: Text('Next'),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
