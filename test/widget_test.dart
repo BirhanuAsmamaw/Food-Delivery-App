df4e37e025a0aa704d21c5da8b9a54607319645a
BirhanuAsmamaw <birhanuasmamaw24@gmail.com>
2024-10-15 02:37:32 +0300

    refactoring code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:food_delivery_app/main.dart';
 
 void main() {
-  
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
@@ -23,6 +22,7 @@ void main() {
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
+
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
